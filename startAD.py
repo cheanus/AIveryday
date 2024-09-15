4813521af99db51d9210da6873af7b5569810384
cheanus <lin.pyw@qq.com>
2024-09-15 12:33:29 +0800

    fix: put wait before request

--- startAD.py
+++ startAD.py
@@ -7,46 +7,51 @@ from GPT import Chat
 from utils import notify
 from ActivityWatcher import ActivityWatcher
 
-def request(args):
-    aw = ActivityWatcher(args)
-    chat = Chat(aw, args)
-    prompt = chat.prompt(args['time']['scope'])
-    if prompt:
-        # 避免各种网络错误
-        try:
-            response = chat.chatgpt(prompt)
-        except Exception as e:
-            logging.error(f"请求出错: {e}")
-            return
-        if response is None:
-            logging.info("跳过此次请求")
-            return
-        logging.info(response)
-        notify(response, args)
+def request():
+    with open('config.yaml', 'r') as f:
+        args = yaml.safe_load(f)
+    
+    if args['power']:
+        aw = ActivityWatcher(args)
+        chat = Chat(aw, args)
+        prompt = chat.prompt(args['time']['scope'])
+        if prompt:
+            # 避免各种网络错误
+            try:
+                response = chat.chatgpt(prompt)
+            except Exception as e:
+                logging.error(f"请求出错: {e}")
+                return
+            if response is None:
+                logging.info("跳过此次请求")
+                return
+            logging.info(response)
+            notify(response, args)
+        else:
+            logging.info("目标未活动")
     else:
-        logging.info("目标未活动")
+        logging.info("您的女友暂时离开了")
+
+def wait():
+    with open('config.yaml', 'r') as f:
+        args = yaml.safe_load(f)
+
+    # 生成一个正态分布的随机时间间隔（以秒为单位）
+    mean = args['time']['delta']['mean']
+    std = args['time']['delta']['std']
+    sleep_time = random.normalvariate(mean, std)
+    sleep_time = max(min(sleep_time, args['time']['delta']['max']),
+                        args['time']['delta']['min'], 0)
+    
+    logging.info(f"下一次运行将在 {sleep_time / 60:.2f} 分钟后")
+
+    # 暂停指定的随机时间
+    time.sleep(sleep_time)
 
 def main():
     while True:
-        with open('config.yaml', 'r') as f:
-            args = yaml.safe_load(f)
-        
-        if args['power']:
-            request(args)
-        else:
-            logging.info("您的女友暂时离开了")
-
-        # 生成一个正态分布的随机时间间隔（以秒为单位）
-        mean = args['time']['delta']['mean']
-        std = args['time']['delta']['std']
-        sleep_time = random.normalvariate(mean, std)
-        sleep_time = max(min(sleep_time, args['time']['delta']['max']),
-                         args['time']['delta']['min'], 0)
-        
-        logging.info(f"下一次运行将在 {sleep_time / 60:.2f} 分钟后")
-
-        # 暂停指定的随机时间
-        time.sleep(sleep_time)
+        wait()
+        request()
 
 if __name__ == '__main__':
     logging.basicConfig(
